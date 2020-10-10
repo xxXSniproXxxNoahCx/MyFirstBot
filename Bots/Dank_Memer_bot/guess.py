68e7402de97835c531ff0c1a37d45c7f6b2ef21d
xxX☆Snipro☆Xxx ☆〜（ゝ。∂）#TheMythicNoahC <59535872+xxXSniproXxxNoahCx@users.noreply.github.com>
2020-10-10 11:58:51 -0400

    Update guess.py

--- Bots/Dank_Memer_bot/guess.py
+++ Bots/Dank_Memer_bot/guess.py
@@ -1,45 +1,49 @@
- import random
+import random
 from telegram.ext import Dispatcher, CommandHandler, ConversationHandler
-
-n = random.randint(1,99)
-tries = 0
-etries = 0
+# n = random.randint(1,99)
+games = {}
 def help():
   return """ 
     猜一个0-100之间的数字。You guessed a number from 0 - 100.
 /DGuess 查看现在的状态和获取帮助。Check your current status and get help.
 /DGuess number 输入number猜数字，看谁用的次数最少。Enter number and see who uses it the least often. """
 def guessing(update, context):
-  global n
-  global tries
+  chatid = update.message.chat.id
+  if not (chatid in games):
+    games[chatid] = {'randomnumber': random.randint(1,99), "members":{}}
+  print(games)
+  fname = update.message.from_user.first_name
   global etries
-  user = update.message.from_user
-  msg8 = ""
-  gn = "亲爱的 %s，请猜一个0-100之间的数字。\nGuess a number from 0 - 100.\n\nLeaderboards: %s - %s\n\n "%(user.first_name,user.username,etries)
-  msg8 += gn
   if len(context.args) == 0 :
     update.message.reply_text(help()) 
     return
-  b = context.args[0]
-  a = int(b)
-  aa = b
-  if a == n :
-    tries = tries + 1
-    if etries == 0 or etries > tries : 
-      etries = etries + tries
-    msg8 += "Ayyy You guessed it! %s guessed it in %s tries. 天呐你竟然猜到了 概率是 '%%1' 呀！ 500 XP %s “它“值得拥有。竟然 %s 次就猜到了."%(user.first_name,tries,user.first_name,tries) 
-    n = random.randint(1,99)
-    tries = 0
-  elif a > n :
-    tries = tries + 1
-    msg8 += "大了！重猜！It's too big! Guess again!"
-  elif a < n :
-    tries = tries + 1
-    msg8 += "小了！重猜！It's too small! Guess again!"
-  elif not aa.isdigit() :
-    tries = tries + 1
-    msg8 += "想什么呢审题呀 ur bad thats not a number"
-  msg8 += "\nTries: %s\n\nAuthorised By Noah <3\n作者：Noah"%(tries)
+  else:
+    b = context.args[0]
+    a = int(b)
+    aa = b
+    if fname in games[chatid]:
+      games[chatid]['members'][fname] += 1
+    else:
+      games[chatid]['members'][fname] = 1
+      msg8 = ""
+      gn = "亲爱的 %s，请猜一个0-100之间的数字。\nGuess a number from 0 - 100.\n\n "%(fname)
+      msg8 += gn
+    if not aa.isdigit() :
+      msg8 += "想什么呢审题呀 ur bad thats not a number"
+    else:
+      if a < 0 or a > 100:
+        msg8 += "\n0-100 \nTries of %s"%(games[chatid]['members'][fname])
+      else:
+        if a == games[chatid]['randomnumber']:
+          if etries == 0 or etries > games[chatid] : 
+            etries = etries + games[chatid]
+          msg8 += "Ayyy You guessed it! %s guessed it in %s tries. 天呐你竟然猜到了 概率是 '%%1' 呀！ 500 XP %s “它“值得拥有。竟然 %s 次就猜到了."%(games[chatid]['members'][fname],games[chatid],games[chatid]['members'][fname],games[chatid]) 
+          games[chatid]['randomnumber'] = random.randint(1,99)
+        elif a > games[chatid]['randomnumber'] :
+          msg8 += "大了！重猜！It's too big! Guess again!"
+        elif a < games[chatid]['randomnumber'] :
+          msg8 += "小了！重猜！It's too small! Guess again!"
+  msg8 += "\nTries: %s\n\nAuthorised By Noah <3\n作者：Noah"%(games[chatid]['members'])
   update.message.reply_text(msg8)
 
 def add_handler(dp:Dispatcher):
